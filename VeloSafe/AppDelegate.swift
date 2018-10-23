5465d54a23af79bac328b9361cd13cd6831758bf
PolinaGuryeva <gurevapolina98@gmail.com>
2018-10-23 12:36:04 +0300

    add map

--- VeloSafe/AppDelegate.swift
+++ VeloSafe/AppDelegate.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let apiKey = "AIzaSyAQvGWJxMwv71xV8WvbvSSZk25E_uoqPEY"
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey(apiKey)
+        GMSPlacesClient.provideAPIKey(apiKey)
         return true
     }
 
--- VeloSafe/ViewController.swift
+++ VeloSafe/ViewController.swift
@@ -7,18 +7,29 @@
 //
 
 import UIKit
+import MapKit
+import GoogleMaps
+import GooglePlaces
 
 class ViewController: UIViewController {
 
     @IBOutlet var seachView: UIView!
+    @IBOutlet weak var mapView: GMSMapView!
     
     @IBOutlet weak var firstPointTextField: UITextField!
     @IBOutlet weak var secondPointTextField: UITextField!
     
+    private var locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    var zoomLevel: Float = 15.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
     }
+    
 
     @IBAction func changeTwoPoints(_ sender: Any) {
         guard let first = firstPointTextField.text,
@@ -28,4 +39,23 @@ class ViewController: UIViewController {
     }
     
 }
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {return}
+        mapView.camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
+                                              longitude: location.coordinate.longitude,
+                                              zoom: zoomLevel)
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        guard status == .authorizedWhenInUse else {return}
+        locationManager.startUpdatingLocation()
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
+    }
+}
+
 
