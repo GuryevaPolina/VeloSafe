1c81b4ff18b289f931c66d2d098e071a5086b492
PolinaGuryeva <gurevapolina98@gmail.com>
2018-10-23 13:31:41 +0300

    add textfields

--- VeloSafe/ViewController.swift
+++ VeloSafe/ViewController.swift
@@ -13,7 +13,8 @@ import GooglePlaces
 
 class ViewController: UIViewController {
 
-    @IBOutlet var seachView: UIView!
+    var searchViewIsOpen = false
+    @IBOutlet var searchView: UIView!
     @IBOutlet weak var mapView: GMSMapView!
     
     @IBOutlet weak var firstPointTextField: UITextField!
@@ -25,17 +26,41 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        initView()
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
     }
     
+    func initView() {
+        mapView.delegate = self
+        
+        searchView.layer.shadowColor = UIColor.lightGray.cgColor
+        searchView.layer.shadowOpacity = 1.0
+        searchView.layer.shadowOffset = .zero
+        searchView.layer.shadowRadius = 3.0
+        searchView.layer.masksToBounds = false
+        
+        searchView.frame.size.height = 128
+        searchView.frame = CGRect(x: 0, y: -searchView.frame.size.height, width: view.frame.size.width, height: searchView.frame.size.height)
+        view.addSubview(searchView)
+    }
 
     @IBAction func changeTwoPoints(_ sender: Any) {
         guard let first = firstPointTextField.text,
             let second = secondPointTextField.text else {return}
         firstPointTextField.text = second
         secondPointTextField.text = first
+    }
+    
+    @objc func moveSearchView() {
+        UIView.animate(withDuration: 0.5) {
+            if self.searchViewIsOpen {
+                self.searchView.frame.origin.y = -self.searchView.frame.size.height
+            } else {
+                self.searchView.frame.origin.y = 0
+            }
+        }
+        searchViewIsOpen = !searchViewIsOpen
     }
     
 }
@@ -55,6 +80,13 @@ extension ViewController: CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
         mapView.isMyLocationEnabled = true
         mapView.settings.myLocationButton = true
+    }
+}
+
+extension ViewController: GMSMapViewDelegate {
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        moveSearchView()
+        searchView.endEditing(true)
     }
 }
 
