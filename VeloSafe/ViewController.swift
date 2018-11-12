e897fad0c869d625cb3490a5c1d61f62784f74bb
PolinaGuryeva <gurevapolina98@gmail.com>
2018-11-12 09:33:21 +0300

    add simple graph to map (google api shapes)

--- VeloSafe/ViewController.swift
+++ VeloSafe/ViewController.swift
@@ -37,53 +37,25 @@ class ViewController: UIViewController {
     }
     
     func drawPath(from gp1: GeoPoint, to gp2: GeoPoint) {
-        let url = "https://maps.googleapis.com/maps/api/directions/json?origin=\(gp1.latitude),\(gp1.longitude)&destination=\(gp2.latitude),\(gp2.longitude)&sensor=false&key=\(apiKey)"
-        let urlRequest = URLRequest(url: URL(string: url)!)
-        let session = URLSession(configuration: URLSessionConfiguration.default)
-        
-        let task = session.dataTask(with: urlRequest) { (data, response, error) in
-            do {
-                guard let json = try JSONSerialization.jsonObject(with: data!, options:[]) as? [String: Any] else {
-                    print("[drawPath] json error")
-                    return
-                }
-                print(json)
-                guard let routes = json["routes"] as? [[String: Any]] else {
-                    print("[drawPath] routes error")
-                    return
-                }
-                for route in routes {
-                    guard let overviewPolyline = route["overview_polyline"] as? [String: Any] else {
-                        print("[drawPath] overview error")
-                        return
-                    }
-                    guard let points = overviewPolyline["points"] as? String else {
-                        print("[drawPath] points error")
-                        return
-                    }
-                    let path = GMSPath(fromEncodedPath: points)
-                    let polyline = GMSPolyline(path: path)
-                    polyline.strokeWidth = 4.0
-                    polyline.strokeColor = .red
-                    polyline.map = self.mapView
-                }
-            }
-            catch {
-                print("json error")
-            }
-        }
-        task.resume()
+        let path = GMSMutablePath()
+        path.addLatitude(gp1.latitude, longitude: gp1.longitude)
+        path.addLatitude(gp2.latitude, longitude: gp2.longitude)
+        let polyline = GMSPolyline(path: path)
+        polyline.strokeWidth = 5.0
+        polyline.geodesic = true
+        polyline.map = mapView
     }
     
     func drawBasicGraph() {
-        drawPath(from: graph[0]!, to: graph[1]!)
         drawPath(from: graph[1]!, to: graph[4]!)
-      //  drawPath(from: graph[0]!, to: graph[3]!)
-      //  drawPath(from: graph[3]!, to: graph[1]!)
-      //  drawPath(from: graph[1]!, to: graph[4]!)
-     //   drawPath(from: graph[4]!, to: graph[5]!)
-      //  drawPath(from: graph[5]!, to: graph[6]!)
-     //   drawPath(from: graph[6]!, to: graph[1]!)
+        drawPath(from: graph[0]!, to: graph[3]!)
+        drawPath(from: graph[3]!, to: graph[1]!)
+        drawPath(from: graph[0]!, to: graph[2]!)
+        drawPath(from: graph[2]!, to: graph[1]!)
+        drawPath(from: graph[1]!, to: graph[4]!)
+        drawPath(from: graph[4]!, to: graph[5]!)
+        drawPath(from: graph[5]!, to: graph[6]!)
+        drawPath(from: graph[6]!, to: graph[1]!)
     }
     
     func initView() {
